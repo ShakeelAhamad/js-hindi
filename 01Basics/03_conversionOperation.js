7cb7464616b6ece8232ae2790f6060946e440ad0
ShakeelAhamad <33038057+ShakeelAhamad@users.noreply.github.com>
2026-01-28 10:44:10 +0000

    Postfix/Prefix  increment

--- 01Basics/03_conversionOperation.js
+++ 01Basics/03_conversionOperation.js
@@ -23,5 +23,49 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//************************* Operations ***************************
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello ";
+let str2 = " Shakeel";
+let str3 = str1 + str2;
+//console.log(str3); //output => "Hello Shakeel"
+
+//console.log("1" + 2); //output => 12
+//console.log(1+"2"); //output => 12
+//console.log("1"+2 + 2); //output => 122
+//console.log(1+2 + "2"); //output => 32
+
+//console.log(+true); //output => 1
+//console.log(+""); //output => 0
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+// console.table([num1,num2,num3]); output=> 4
+
+let  gameCounter = 100
+gameCounter++;
+//console.log(gameCounter);// output => 101
+let x = 3;
+let y = x++;
+//console.log("Y:",y); // Output => 3
+//console.log("X:",x); // Output => 4
+let P = 3;
+let M = ++P;
+//console.log("P:",P);//Output => 4
+//console.log("M:",M); // Output => 4
+ 
